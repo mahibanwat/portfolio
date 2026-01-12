178cef324fcabc5b7a8611401b0797d0780b3e73
mahi banwat <mahibanwat@gmail.com>
2026-01-12 11:11:32 +0530

    kuch nahi

---
+++ calculator/index.js
@@ -0,0 +1,14 @@
+// let student ={
+//     name:'mahi',
+//      age:19,
+// };
+// console.log(typeof student["age"]);
+
+// const collegeStudent={
+//     marks:48,
+// };
+// console.log('collegeStudent');
+let a=10;
+let b=45;
+console.log(a+b);
+console.log(a%b);
